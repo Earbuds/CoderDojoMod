5f9cb6a35d845705b51392dfb143f19e502a5b7d
dluxcru <daviddale.webmaster@gmail.com>
2015-11-21 21:21:40 -0600

    TileEntities Documented
    Added documentation to the TileEntity framework.

--- src/main/java/org/coderdojostc/common/block/CDBlock.java
+++ src/main/java/org/coderdojostc/common/block/CDBlock.java
@@ -3,6 +3,7 @@ package org.coderdojostc.common.block;
 import org.coderdojostc.common.core.CreativeTabCD;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.Block.SoundType;
 import net.minecraft.block.material.Material;
 
 /**
@@ -12,17 +13,23 @@ import net.minecraft.block.material.Material;
 public class CDBlock extends Block {
 	/**
 	 * 
-	 * @param material Material for the block
-	 * @param hardness hardness of the material
-	 * @param stepsound sound when walking
-	 * @param blockname block name in game
+	 * @param material The material type of the block.
+	 * @param sound The sound the block makes when walking on it, digging etc.
+	 * @param blockname The name of the block in your inventory.
+	 * @param hardness How long the block takes to break.
+	 * @param resistance Blast resistance of block.
+	 * @param harvestTool Tool used to harvest this block.
+	 * @param harvestLevel The harvest level of the block.
 	 */
-	public CDBlock(Material material, float hardness, SoundType stepsound, String blockname) {
+	public CDBlock(Material material, SoundType sound, String blockname,
+			float hardness, float resistance, String harvestTool, int harvestLevel) {
 		super(material);
-		setHardness(hardness);
-		setStepSound(stepsound);
-		setBlockName(blockname);
-		setCreativeTab(CreativeTabCD.tabCD);
+		this.setStepSound(sound);
+		this.setBlockName(blockname);
+		this.setHardness(hardness);
+		this.setResistance(resistance);
+		this.setHarvestLevel(harvestTool, harvestLevel);
+		this.setCreativeTab(CreativeTabCD.tabCD);
 	}
 	
 }
--- src/main/java/org/coderdojostc/common/block/CDTileEntityContainer.java
+++ src/main/java/org/coderdojostc/common/block/CDTileEntityContainer.java
@@ -1,22 +1,43 @@
 package org.coderdojostc.common.block;
 
+import org.coderdojostc.common.core.CreativeTabCD;
+
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
+/**
+ * @author DLuxCru
+ *The CDTileEntityContainer class functions
+ * as something of a link between blocks and their associated tile entities.
+ */
 public class CDTileEntityContainer extends BlockContainer {
-
-	public CDTileEntityContainer(Material material_, float hardness, float resistance, String harvestTool, int harvestLevel){
-		super(material_);
+	/**
+	 * 
+	 * @param material The material type of the block.
+	 * @param sound The sound the block makes when walking on it, digging etc.
+	 * @param blockname The name of the block in your inventory.
+	 * @param hardness How long the block takes to break.
+	 * @param resistance Blast resistance of block.
+	 * @param harvestTool Tool used to harvest this block.
+	 * @param harvestLevel The harvest level of the block.
+	 */
+	public CDTileEntityContainer(Material material, SoundType sound, String blockname,
+			float hardness, float resistance, String harvestTool, int harvestLevel){
+		super(material);
 		// TODO Auto-generated constructor stub
-		String placeholder = "";
-		//this.setUnlocalizedName(placeholder);
+		this.setStepSound(sound);
+		this.setBlockName(blockname);
 		this.setHardness(hardness);
 		this.setResistance(resistance);
 		this.setHarvestLevel(harvestTool, harvestLevel);
+		this.setCreativeTab(CreativeTabCD.tabCD);
+	
 	}
-
+	/**
+	 * @return returns a tile entity of the CDTileEntity type.
+	 */
 	@Override
 	public TileEntity createNewTileEntity(World p_149915_1_, int p_149915_2_) {
 		// TODO Auto-generated method stub
