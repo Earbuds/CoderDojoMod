96b22cc382c5634ff30d9de2d3ab1280c58f7a87
dluxcru <daviddale.webmaster@gmail.com>
2015-12-05 19:49:44 -0600

    BookBerryBush class
    Used to create the book berry bush in

---
+++ src/main/java/org/coderdojostc/common/block/BookBerryBush.java
@@ -0,0 +1,13 @@
+package org.coderdojostc.common.block;
+
+import net.minecraft.server.gui.IUpdatePlayerListBox;
+
+public class BookBerryBush extends CDTileEntity implements IUpdatePlayerListBox{
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
